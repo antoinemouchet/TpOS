93659d8f2cd8fcf4f5ab9040d54e421fe0f69de0
Antoine Mouchet <antoine.mouchet@gmail.com>
2019-10-20 20:41:27 +0200

    checking if dict is empty

--- TP3/exo4/dict.c
+++ TP3/exo4/dict.c
@@ -1,9 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 struct dict_element
 {
-    //Each element of dictionary has a name (= word itself) and a definition.
+    //Word itself and its definition
     char word[100];
-    char definition[500];
+    char definition[500]; 
 
     struct dict_element *next ;
 
 };
+
+int isEmpty(struct dict_element* head)
+{
+    //Initialize nb of elements in struct
+    int NbElements = 0;
+
+    //Initialize current position of pointer
+    struct dict_element *PosPoint = head;
+
+    //Loop on the 
+    while (PosPoint != NULL)
+    {
+        //Increment nb of elements and move pointer
+        NbElements ++;
+        PosPoint = PosPoint -> next;
+    }
+
+    /*Check if dict is empty
+    Return a positive integrer if yes. */
+    if (NbElements == 0)
+    {
+        return 1;
+    }
+    
+    //Return 0 if dict is not empty
+    return 0;
+}
