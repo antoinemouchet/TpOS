8a4a94c0c12abc588632f9ce1ef715b369d191b6
Antoine Mouchet <antoine.mouchet@gmail.com>
2019-10-24 10:27:47 +0200

    Tp4ex5 first try

---
+++ TP4/exo5/FamilyTree.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void displayGenerationError()
+{
+    printf("There was an error generating new process.\n");
+}
+
+int main(int argc, char const *argv[])
+{
+    //Generate first child
+    int n1 = fork(); 
+  
+    //Child case
+    if (n1 == 0)
+    {
+        printf("I'm the first child. My pid is: %d. \
+        Parent's pid is: %d.\n", getpid(), getppid());
+
+        //Generate first child of first child
+        int n14 = fork();
+
+        //Child case
+        if (n14 == 0)
+        {
+            printf("I'm the fourth child. My pid is: %d. \
+            Parent's pid is: %d.\n", getpid(), getppid());
+
+            //Child of child of child
+            int n147 = fork();
+            if (n147 == 0)
+            {
+                printf("I'm the seventh child. My pid is: %d. \
+                Parent's pid is: %d.\n", getpid(), getppid());
+            }
+            //Error
+            else if (n147 < 0)
+            {
+                displayGenerationError();
+            }
+        }
+
+        //Parent case
+        else if (n14 > 0)
+        {
+            //Child of child
+            int n15 = fork();
+
+            //Child case
+            if (n15 == 0)
+            {
+                printf("I'm the fifth child. My pid is: %d. \
+                Parent's pid is: %d.\n", getpid(), getppid());
+            }
+            //Error
+            else if (n15 < 0)
+            {
+                displayGenerationError();
+            }
+        }
+
+        //Error case
+        else
+        {
+            displayGenerationError();
+        }
+        
+    } 
+    //Parent case in first child
+    else if (n1 > 0)
+    {
+        //Second child
+        int n2 = fork(); 
+
+        //Child case
+        if (n2 == 0)
+        { 
+            printf("I'm the second child. My pid is: %d. \
+            Parent's pid is: %d.\n", getpid(), getppid());
+            //Child of child
+            int n16 = fork();
+            
+            //Child case
+            if (n16 == 0)
+            {
+                printf("I'm the sixth child. My pid is: %d. \
+                Parent's pid is: %d.\n", getpid(), getppid());
+            }
+            //Error
+            else if (n16 < 0)
+            {
+                displayGenerationError();
+            }
+        }
+
+        //Parent case in first and second child
+        else if (n2 > 0)
+        {
+            //Third child
+            int n3 = fork(); 
+            if (n3 == 0)
+            { 
+                printf("I'm the third child. My pid is: %d. \
+                Parent's pid is: %d.\n", getpid(), getppid()); 
+            } 
+  
+            //Parent process
+            else if (n3 > 0)
+            { 
+                printf("I'm the father, my pid is: %d\n", getpid());
+            }
+            //Error case
+            else
+            {
+                displayGenerationError();
+            }
+        }
+    }
+    else
+    {
+        displayGenerationError();
+    }
+    
+    return 0;
+}
