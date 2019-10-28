04a7a29a8b315091575ea2bd6db8b7c9341edb52
Simon Willame <simon.willame2@gmail.com>
2019-10-28 15:24:27 +0100

    TP4 exo7 process version

---
+++ TP4/exo7/monshell.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+
+
+pid_t create_process(void)
+{
+    //create a process
+    pid_t pid;
+
+
+    do {
+	pid = fork();
+    } while ((pid == -1) && (errno == EAGAIN));
+
+
+    return pid;
+}
+
+
+int main(int argc, char const *argv[])
+{
+    char exit_key[] = "quit";
+    int quit = 0;
+    while (quit = 0)
+    {
+        
+        char commande[300];
+        
+        // ask the command 
+        printf("> ");
+        scanf("%s", commande);
+        
+        //check if the user want to leave monshell
+        if (strcmp(commande, exit_key) == 0)
+        {
+            int quit = 1;
+        }
+        
+        //run programm
+        else
+        { 
+            pid_t son = create_process();
+            
+            switch (son)
+            {   //error case
+                case -1:
+                    return EXIT_FAILURE;
+                //son case
+                case 0:
+                    system(commande);
+                    break;
+                //father case
+                default:
+                    wait(NULL);
+                    break;
+            }
+        }
+        
+    }
+    
+    
+    return 0;
+}
+
