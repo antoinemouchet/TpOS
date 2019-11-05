62016520c4ffb305981fe99ed47a61d95a09c966
Antoine Mouchet <antoine.mouchet@gmail.com>
2019-11-05 23:29:54 +0100

    Tp5ex4 Trying

--- TP5/exo4/nInteger_mSon.c
+++ TP5/exo4/nInteger_mSon.c
@@ -6,95 +6,151 @@
 #include <errno.h>
 #include <sys/types.h>
 
+int FinalResult = 0;
+
+void handler(int signum, siginfo_t *info, void *ucontext)
+{
+    // Add computed sum to Final result
+    FinalResult += info -> si_value.sival_int;
+
+    // Display which child sent this sum
+    printf("The child with pid %d sent the result: %d.\n", info -> si_pid, info -> si_value.sival_int);
+}
+
+int ComputeSum(int StartValue, int EndValue)
+{
+    // Initialize result of sum to 0
+    int sum = 0;
+
+    // Loop to get all the integers at sum
+    for (int i = StartValue; i < EndValue; i++)
+    {
+        // Add number to sum
+        sum += i;
+    }
+
+    printf("Child with pid (%d) computed sum from %d to %d.\nThe result was: %d.\n", getpid(), StartValue, EndValue, sum);
+    // Return the sum
+    return sum;
+}
+
+
 
 int main(int argc, char const *argv[])
 {
-    int  id, m, n, remember_me, division, start_value, end_value, total;
+    int  id = 0, division, start_value = 0, end_value = 0, total = 0;
 
-    int identificator[500];
+    int m, n, forkReturn;
 
-    //get number to sum to
+    struct sigaction act;
+            
+    // Associating a function so we use sigaction
+    act.sa_sigaction = &handler;
+
+    // Using this flag with real-time signal allow to obtain data from si_value from siginfo_t struct
+    act.sa_flags = SA_SIGINFO;
+
+    // SIGRTMIN is a real-time signal. The advantage is that they'll queue up when multiple are sent
+    int success = sigaction(SIGRTMIN, &act, NULL);
+
+    // Error while modifyinf 
+    if (success == -1)
+    {
+        perror("An error occured: ");
+        // Error
+        return 1;
+    }
+
+    /* IF WE WANT TO GET INFORMATIONS DIRECTLY FROM THE COMMAND LINE USE THIS METHOD
+    if (argc != 3)
+    {
+        printf("Wrong amount of arguments, insert the number to reach and the amount of child process to create.\n");
+        // Error
+        return 1;
+    }
+    // Convert string to integer using atoi()
+    // Get number to reach.
+    n = atoi(argv[1]);
+
+    // Get number of child process to create
+    m = atoi(argv[2]);
+    */
+
+    // SCANF method
+    // Get number to sum to
     printf("Write an int.\n");
     scanf("%d", &n);
 
-    //Get the number of child process
+    // Get the number of child process
     printf("Write the number of son, it must be an int.\n");
     scanf("%d", &m);
-
-    id = 0;
-    start_value = 0;
-    end_value = 0;
-    division = n/m;
-    total = 0;
-
-    //looping to create sons
-    for (int i = 0; i <= m && id == 0; i++)
+    
+   // Get division
+    division = n / m;
+ 
+    // Looping to get all sums (by creating sons)
+    for (int i = 0; i < m && id == 0; i++)
     {   
-        // Choosing right values for the son
-        
-        // Get last int used by the son (or 0 at first)
-        start_value = end_value;
-        
-        // Last son won't be equal to the other sometimes
-        if (i == m)
-            end_value = n;
-        else
-        {
-            // add to be equal
-            end_value += division;
-        }
-        
-        identificator[i] = fork();
+        //Create a child
+        forkReturn = fork();
 
-        //Son case
-        if (identificator[i] == 0)
+        // Son case
+        // Here we want to compute a sum
+        if (forkReturn == 0)
         {
-            remember_me = i;
+            int result;
+            // Declare structure to send 
+            union sigval value;
+
+            // Check that it's not the last segment
+            if (i < m - 1)
+            {
+                // Get the sum of each segment
+                result = ComputeSum(i * division, division * (i + 1));
+            }
+
+            // Last segment, get sum to last number
+            else
+            {
+                // The sum of last segment is till n is reached
+                result = ComputeSum(i * division, n);
+            }
+
+            // Store result of the sum
+            value.sival_int = result;
+
+            // Get parent pid
+            int parentPid = getppid();
+
+            //Display and send informations to parent process
+            printf("Child (pid: %d) sends %d to its parent (pid: %d).\n", getpid(), result, parentPid);
+            sigqueue(parentPid, SIGRTMIN, value);
+            
+            // Make sure to leave loop in this process.
             id = 1;
+
+            // Kill child - Suicide
+            kill(getpid(), SIGKILL);
         }
         
-        //Error case
-        else if (identificator[i] < 0)
+        // Error case
+        else if (forkReturn < 0)
         {
             perror("There was an error generating the process: ");
+            // Make sure to leave loop
             id = -1;
             return 1;
-        }    
+        }
 
-    }
-
-    //Add integer 
-    
-    switch (remember_me)
-    {
-        //son case    
-        case 0:
-            int result;
-            result = 0;
-            start_value++;
-
-            //add integer
-            for (start_value; start_value <= end_value; start_value++)
-            {
-                result += start_value;
-            }
-            //send result to the father
-            sigqueue(getppid(),0, result);
-            
-            break;
-        
-        //father case
-        default:
-            for (int i = 0; i <= m; i++)
-            {
-                //waiting the son's result
-                wait(NULL);
-                total += result; 
-            }
-            //TO COMPLETE
-
-            break;
+        // Parent case
+        else
+        {
+            // Pause to wait each child in parent process
+            pause();
+        }
     }
     
+    // Display final result
+    printf("The sum to %d is: %d.\n", n, FinalResult);
     return 0;
 }
