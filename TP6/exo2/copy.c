90cd63c80e9d8962ab72a65fa9807253d6796f37
Antoine Mouchet <antoine.mouchet@gmail.com>
2019-11-08 12:08:29 +0100

    Tp6ex2 start

---
+++ TP6/exo2/copy.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h> 
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#define FILE_SIZE 1024
+
+int main(int argc, char const *argv[])
+{
+    // Check that the good amount of arguments was given.
+    if (argc != 3)
+    {
+        printf("Wrong amount of arguments. 2 doc names waited.\n");
+        // Error case
+        return 1;
+    }
+
+    // Get names of files from the arguments passed in the command line.
+    char fileName1[] = argv[1];
+    char fileName2[] = argv[2];
+
+
+    return 0;
+}
