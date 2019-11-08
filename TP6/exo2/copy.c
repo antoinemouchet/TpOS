525d0ca1fa612b5eecb43c9c251715e1af5423a7
Antoine Mouchet <antoine.mouchet@gmail.com>
2019-11-08 12:09:18 +0100

    Tp6ex2 fixing include

--- TP6/exo2/copy.c
+++ TP6/exo2/copy.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h> 
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h> 
+
 
 #define FILE_SIZE 1024
 
