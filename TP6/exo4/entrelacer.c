453d3be5d5a1f53a39ad9e6ae62460650e2eef53
Antoine Mouchet <antoine.mouchet@gmail.com>
2019-11-10 20:21:59 +0100

    tp6ex4 start

---
+++ TP6/exo4/entrelacer.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include<unistd.h>
+#include <fcntl.h> 
+#include <errno.h>
+//#include <sys/wait.h>
+#include <signal.h>
+
+
+int main(int argc, char const *argv[])
+{
+    
+
+    return 0;
+}
