b3745e9bc08492f55fbeedfe200525b596ec0a11
Antoine Mouchet <antoine.mouchet@gmail.com>
2019-11-11 15:02:12 +0100

    tp6ex3 test

--- TP6/exo3/reading_father_son.c
+++ TP6/exo3/reading_father_son.c
@@ -28,7 +28,7 @@ void PrintCharInfFile(int fileDescriptor, int number, char *filename)
     for (i = 0; i < number && i < nbread; i++)
     {
         // Move pointer to get next character
-        sentence[i] = (char) getc((char)(buffer + i));
+        sentence[i] = (char) getc(filename + i);
     }
     // Add EOF char
     sentence[i+1]='\0';
@@ -79,6 +79,7 @@ int main(int argc, char const *argv[])
         PrintCharInfFile(fileDescriptor, 10, file);
         break;
     }
+    close(fileDescriptor);
     return 0;
 }
 
