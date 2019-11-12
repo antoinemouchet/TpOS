4a7d0a2bab9f948b65e2ca6a30b644805f011bdd
Simon Willame <simon.willame2@gmail.com>
2019-11-12 16:13:30 +0100

    TP6 exo  + makefile done ready to test

---
+++ TP6/exo5/CharInFile.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h> 
+#define buffer 1024
+
+int main(int argc, char const *argv[])
+{
+    int KeepRolling = 0;
+    int sentence[250];
+    char StopRolling[5] = "quit";
+    int fd = open("./ReadHereMySon", O_RDWR|O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+    while(KeepRolling == 0)
+    {
+        // Ask sentence to write in the file
+        printf("write something\n");
+        scanf("%s", sentence);
+        // Check the user want to leave the programm
+        if (sentence == StopRolling)
+        {
+            KeepRolling = 1;
+        }
+        else
+        {   
+            //write sentence into fd
+            int w = write(fd, buffer, sentence);
+            
+            //Check if written correctly
+            if (w == -1)
+            {
+                printf("error");
+                return 1;
+            } 
+            
+        }
+        
+        //wc 
+        int MySonIsReading = fork();
+            switch (MySonIsReading)
+            {
+            case -1:
+                perror("fork");
+                return 2;
+            
+            //son case
+            case 0: 
+                //using wc linux command
+                char command[65];
+                
+                strcpy(command, "wc ReadHereMySon"); /* .txt ? */
+                system(command);
+            default:
+                wait(NULL);
+                break;
+            }
+    
+    //Close file
+    close(fd);
+    }
+    return 0;
+}
---
+++ TP6/exo6/lancer.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h> 
+
