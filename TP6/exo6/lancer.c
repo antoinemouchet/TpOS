ccddc32f3df0972dec66508bb1e3a9e8a79a8fd0
Antoine Mouchet <antoine.mouchet@gmail.com>
2019-11-13 11:27:35 +0100

    tp6ex6 adding enonce in file

--- TP6/exo6/lancer.c
+++ TP6/exo6/lancer.c
@@ -9,3 +9,14 @@
 //#include <sys/wait.h>
 
 
+/*Exercice 6* Écrivez un programme lancer qui crée un processus ﬁls.
+Le processus ﬁls exécute le programme qui est passé en argument au programme lancer.
+Le résultat de l’exécution du programme ou commande est stocké dans un ﬁchier qui est aussi passé en argument.
+Pour des erreurs de l’exécution, il faut qu’elles soit aﬃchées sur l’écran
+Le programme lancer a un format suivant : lancer commande [arg1, arg2, ...] fichier_sortie
+
+Exemple: $ ./lancer ls . /etc resultat 
+$ ./lancer cp resultat resultat2 sortie
+
+$ ./lancer toto resultat
+Program or command toto is not found*/
