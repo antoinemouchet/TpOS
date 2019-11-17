e9230ee7e160cb519febe125f60e8bdd45b635ab
Simon Willame <simon.willame2@gmail.com>
2019-11-17 19:07:41 +0100

    TP7 exo 2 + Makefile ready for the test

---
+++ TP7/exo2/NoWriteOrRead.c
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include<unistd.h>
+#include <fcntl.h> 
+#include <errno.h>
+
+int main(int argc, char const *argv[])
+{
+    int pid, fpipe[2];
+    //create pipe
+    if (pipe(fpipe) == 0)
+    {
+        
+        //make ls and sort at the same time
+        pid = fork();
+        
+        switch (pid)
+        {
+            case -1:
+                perror("fork");
+
+                return 1;
+            case 0:
+                //son case 
+                //execute ls 
+                //reading the file in the pipe
+                close(fpipe[0]);
+                // receive the input 
+                dup2(fpipe[1], STDOUT_FILENO); 
+                execlp("ls", "ls");
+                //close pipe
+                close(fpipe[1]);
+                break;
+            default:
+                //father case
+                //execute sort -d                
+                int ftxt;
+                //writing in the pipe
+                close(fpipe[1]);
+
+                ftxt = open("file.txt", O_WRONLY);
+                // send an input in the pipe 
+                dup2(fpipe[0], STDIN_FILENO); 
+                //send file.txt to the son in the pipe 
+                dup2(ftxt, STDOUT_FILENO); 
+                execlp("sort", "sort", "-d");
+                
+                
+                //Close file and pipe 
+                close(fpipe[0]);
+                close(ftxt);
+                
+                break;
+    
+        }
+    }
+
+    else
+    {
+        perror("pipe");
+        return 2;
+    }
+    return 0;
+    // RETURN 0 IF SUCESS, 1 IF FORK ERROR, 2 IF PIPE ERROR
+}
