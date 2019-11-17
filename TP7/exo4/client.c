1bd678f8d74737fdf6850262ec97bb20429a5f63
Simon Willame <simon.willame2@gmail.com>
2019-11-18 00:02:06 +0100

    TP7 exo4 client.c unfinished

--- TP7/exo4/client.c
+++ TP7/exo4/client.c
@@ -14,6 +14,41 @@
 
 int main(int argc, char const *argv[])
 {
+    int requestPipe, resultPipe, requestTag;
+    requestPipe = open(SEND_REQ, O_WRONLY);
+    resultPipe = open(GET_RES, O_RDONLY);
+    // check if it reached the server
+    if ((requestPipe == -1) || (resultPipe == -1))
+    {
+        printf("couldn't reach the server");
+        return 1;
+    }
+    printf("server has been reached");
+    //start request
+    do
+    {
+        printf("choose one request:\n 0. Add a word + definition\n 1. Remove a word \n 2. Select a word from dict\n 3. Exit\n >");
+        scanf("%d", &requestTag);
+        switch (requestTag)
+        {
+            //Add a word
+            case 0:
+                printf("you choose to add a word in the dict.");
+                break;
+            //Remove a word from dict
+            case 1:
+                printf("you choose to remove a word from dict.");
+                break;
+            //select a word from dict
+            case 2:
+                printf("you choose to select a word from dict.");
+                break;
+            //leaving process
+            case 3:
+                printf("you choose to exit");
+                break;
+        }
+    } while (requestTag < 3);
     
     return 0;
 }
