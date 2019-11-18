64586c1554c9b5f686759b0f4fada4d40171ab05
Simon Willame <simon.willame2@gmail.com>
2019-11-18 15:50:44 +0100

    TP7 exo4 client.c Ready to test

--- TP7/exo4/client.c
+++ TP7/exo4/client.c
@@ -13,7 +13,11 @@
 
 
 int main(int argc, char const *argv[])
-{
+{   
+    char word[128];
+    char definition[512];
+    char requestSentence[1024];
+    char resultSentence[1024];
     int requestPipe, resultPipe, requestTag;
     requestPipe = open(SEND_REQ, O_WRONLY);
     resultPipe = open(GET_RES, O_RDONLY);
@@ -27,6 +31,7 @@ int main(int argc, char const *argv[])
     //start request
     do
     {
+        // ASK A REQUEST
         printf("choose one request:\n 0. Add a word + definition\n 1. Remove a word \n 2. Select a word from dict\n 3. Exit\n >");
         scanf("%d", &requestTag);
         switch (requestTag)
@@ -34,21 +39,58 @@ int main(int argc, char const *argv[])
             //Add a word
             case 0:
                 printf("you choose to add a word in the dict.");
+                //choose a word
+                printf("write a word: ");
+                scanf("%s", word);
+                //choose his definition
+                printf("write its definition: ");
+                scanf("%s", definition);
+
+                // Init to send word and definition and adding tag in the pipe
+                // requestSentence is written Tag:word:its definition
+                sprintf(requestSentence, "%d:%s:%s", &requestTag, word, definition);
+                printf("Request send");
                 break;
+
             //Remove a word from dict
             case 1:
                 printf("you choose to remove a word from dict.");
+                printf("choose a word to delete: ");
+                scanf("%s", word);
+
+                // Init to send the word to delete and deleting tag in the pipe
+                sprintf(requestSentence, "%d:%s", &requestTag, word);
+                printf("Request send");
                 break;
             //select a word from dict
             case 2:
                 printf("you choose to select a word from dict.");
+                printf("Wich word want you to know ? ");
+                scanf("%s", word);
+                // Init to send the word to find and finding tag in the pipe
+                sprintf(requestSentence, "%d:%s", &requestTag, word);
+                printf("Request send");
                 break;
             //leaving process
             case 3:
                 printf("you choose to exit");
                 break;
         }
+        // Send requestSentence in the pipe 
+        write(requestPipe, requestSentence, strlen(requestSentence));
+        
+
+        // RECIEVE RESULT 
+        // Don't wait for a result if  requestTag >=3
+        if(requestTag < 3)
+        {
+            read(resultPipe, resultSentence, BUF_SIZE);
+            printf("%s", resultSentence);
+        }
+        
     } while (requestTag < 3);
-    
+    //closing pipe
+    close(resultPipe);
+    close(requestPipe);
     return 0;
 }
