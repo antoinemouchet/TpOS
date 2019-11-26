38debfbd72f3a2f07da9cda62eb0a8c88874289a
Antoine MOUCHET <amouchet@babbage32.pool.fundp.ac.be>
2019-11-26 13:47:29 +0100

    tp8 work in progress and makefile

--- TP8/diner.c
+++ TP8/diner.c
@@ -1,15 +1,21 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <pthread.h>
 #include <unistd.h>
 
 #define NB_PHILO 5
 #define FREE 0
 #define TAKEN 1
+//#define MAX_EATING 20
 
-pthread_mutex_t  table;
+// Global lock
+pthread_mutex_t  eating;
 
+// Global variable for 
+int forkStatus[NB_PHILO];
+
+//int eaten = 0;
 
 // Get id of the left fork for a philosophe
 int leftFork(int philId)
@@ -26,28 +32,58 @@ int rightFork(int philId)
 
 void takeForks(int philId)
 {
-
+    // Update status of forks, "giving" them to the philosophe
+    forkStatus[leftFork(philId)] = TAKEN;
+    forkStatus[rightFork(philId)] = TAKEN;
 }
 
 void freeForks(int philId)
 {
-
+    // Update status of forks, making them free
+    forkStatus[leftFork(philId)] = FREE;
+    forkStatus[rightFork(philId)] = FREE;
 }
 
-void think()
+void think(int philID)
 {
-    printf("I'm thinking about something.\n");
+    // Display thinking message
+    printf("I'm thinking about something. (Philosophe: %d)\n", philID);
+}
+
+void eat(int philID)
+{
+    // Check if philosophe has the forks in the hands
+    if (forkStatus[rightFork(philID)] == TAKEN && forkStatus[leftFork(philID)] == TAKEN)
+    {
+        // Display eating message
+        printf("%d is eating.\n", philID);
+
+        // Increase nb of times philosophes ate
+        //eaten ++;
+    }
 }
 
 void* philAction(int philId)
 {
+    // Infinite loop
     while (1)
     {
-        think();
-        lock(table);
+        // Thinking phase
+        think(philId);
 
+        // Critical zone
+        pthread_mutex_lock(&eating);
+
+        // Take forks to eat
+        takeForks(philId);
+        // Eat
+        eat(philId);
+        // Release forks 
+        freeForks(philId);
+
+        // End of critical zone
+        pthread_mutex_unlock(&eating);
     }
-    
 }
 
 int main(int argc, char const *argv[])
@@ -58,7 +94,7 @@ int main(int argc, char const *argv[])
     pthread_t philosopheIds[NB_PHILO];
 
     // Create mutex
-    mutRet = pthread_mutex_init(&table, NULL);
+    mutRet = pthread_mutex_init(&eating, NULL);
 
     // Check that mutex was correctly initialized
     if (mutRet != 0)
@@ -78,14 +114,18 @@ int main(int argc, char const *argv[])
         if (threadRet != 0)
         {
             // Error
-            // strerror returns a string describing the error
-            strerror(threadRet);
+            printf("Error creating thread.\n");
             return 1;
         }
-        
-
     }
-    
+    // Loop to end all threads
+    for (int i = 0; i < NB_PHILO; i++)
+    {
+        pthread_join(&philosopheIds[i], NULL);
+    }
+
+    // Destroy mutex
+    pthread_mutex_destroy(&eating);
     
 
     return 0;
